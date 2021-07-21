fe926fd3c42f62508a371923c477e7a4e9680a42
scappworks <scappworks@gmail.com>
2021-07-21 01:31:06 -0400

    add scope and callback method

--- app/src/main/java/com/scappworks/weeklyplanner/PlannerApplication.kt
+++ app/src/main/java/com/scappworks/weeklyplanner/PlannerApplication.kt
@@ -3,8 +3,11 @@ package com.scappworks.weeklyplanner
 import android.app.Application
 import com.scappworks.weeklyplanner.roomdb.DB
 import com.scappworks.weeklyplanner.roomdb.DbRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class PlannerApplication : Application() {
-    val database by lazy { DB.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { DB.getDatabase(this, applicationScope) }
     val repository by lazy { DbRepository(database.weekdayDao()) }
 }
--- app/src/main/java/com/scappworks/weeklyplanner/roomdb/DB.kt
+++ app/src/main/java/com/scappworks/weeklyplanner/roomdb/DB.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.scappworks.weeklyplanner.roomdb.weekdaytable.Weekday
 import com.scappworks.weeklyplanner.roomdb.weekdaytable.WeekdayDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Weekday::class), version = 1, exportSchema = false)
 public abstract class DB : RoomDatabase() {
@@ -17,7 +20,7 @@ public abstract class DB : RoomDatabase() {
         @Volatile
         private var INSTANCE: DB? = null
 
-        fun getDatabase(context: Context): DB {
+        fun getDatabase(context: Context, scope: CoroutineScope): DB {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -25,11 +28,46 @@ public abstract class DB : RoomDatabase() {
                     context.applicationContext,
                     DB::class.java,
                     "word_database"
-                ).build()
+                ).addCallback(PlannerDatabaseCallback(scope)).build()
                 INSTANCE = instance
                 // return instance
                 instance
             }
+        }
+    }
+
+    private class PlannerDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.weekdayDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(weekdayDao: WeekdayDao) {
+            // Delete all content here.
+            weekdayDao.deleteAllWeekdays()
+
+            // Add weekdays
+            var weekday = Weekday(0, "Sunday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Monday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Tuesday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Wednesday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Thursday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Friday")
+            weekdayDao.insert(weekday)
+            weekday = Weekday(0, "Saturday")
+            weekdayDao.insert(weekday)
         }
     }
 }
