9e1c83d1e6df799d401526e94afdd86a8fef8f86
scappworks <scappworks@gmail.com>
2021-07-21 01:38:13 -0400

    add viewmodel to main activity for observing weekdays list

--- app/src/main/java/com/scappworks/weeklyplanner/MainActivity.kt
+++ app/src/main/java/com/scappworks/weeklyplanner/MainActivity.kt
@@ -2,10 +2,22 @@ package com.scappworks.weeklyplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.scappworks.weeklyplanner.viewmodel.PlannerViewModel
+import com.scappworks.weeklyplanner.viewmodel.PlannerViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private val plannerViewModel: PlannerViewModel by viewModels {
+        PlannerViewModelFactory((application as PlannerApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        plannerViewModel.allWeekdays.observe(this, Observer { weekdays ->
+            weekdays.let { }
+        })
     }
 }
