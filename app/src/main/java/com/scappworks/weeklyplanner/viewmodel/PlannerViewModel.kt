76bc52d9ee62bc04f7bb24e13c50f06598ca5d9a
scappworks <scappworks@gmail.com>
2021-07-21 01:14:30 -0400

    add viewmodel

---
+++ app/src/main/java/com/scappworks/weeklyplanner/viewmodel/PlannerViewModel.kt
@@ -0,0 +1,25 @@
+package com.scappworks.weeklyplanner.viewmodel
+
+import androidx.lifecycle.*
+import com.scappworks.weeklyplanner.roomdb.DbRepository
+import com.scappworks.weeklyplanner.roomdb.weekdaytable.Weekday
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class PlannerViewModel(private val repository: DbRepository) : ViewModel() {
+    val allWeekdays: LiveData<List<Weekday>> = repository.allWeekdays.asLiveData()
+
+    fun insert(weekday: Weekday) = viewModelScope.launch {
+        repository.insert(weekday)
+    }
+}
+
+class PlannerViewModelFactory(private val repository: DbRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PlannerViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PlannerViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
