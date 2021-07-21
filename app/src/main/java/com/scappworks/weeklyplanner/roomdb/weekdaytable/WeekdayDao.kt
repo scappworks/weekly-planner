59a8d6f6c73b9dc21bb5f2fc35ccd583e7070960
scappworks <scappworks@gmail.com>
2021-07-21 00:58:22 -0400

    change return type of getWeekdays() to Flow List

--- app/src/main/java/com/scappworks/weeklyplanner/roomdb/weekdaytable/WeekdayDao.kt
+++ app/src/main/java/com/scappworks/weeklyplanner/roomdb/weekdaytable/WeekdayDao.kt
@@ -4,11 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeekdayDao {
     @Query("SELECT * FROM weekday ORDER BY id ASC")
-    fun getWeekdays(): List<Weekday>
+    fun getWeekdays(): Flow<List<Weekday>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(day: Weekday)
