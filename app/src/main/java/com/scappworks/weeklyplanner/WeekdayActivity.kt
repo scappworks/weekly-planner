081abf11f8974b2c1fab7168510986e34aeeb26a
scappworks <scappworks@gmail.com>
2021-07-23 01:08:53 -0400

    create weekday activity

---
+++ app/src/main/java/com/scappworks/weeklyplanner/WeekdayActivity.kt
@@ -0,0 +1,11 @@
+package com.scappworks.weeklyplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeekdayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weekday)
+    }
+}
