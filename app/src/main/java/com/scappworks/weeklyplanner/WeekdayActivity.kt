8dbd9a5696926f2e1f7de9996b843cf9708d17ff
scappworks <scappworks@gmail.com>
2021-07-23 23:59:03 -0400

    within the weekday viewmodel open a task viewmodel and determine if the task being looked at belongs in the list for that days task, add comments

--- app/src/main/java/com/scappworks/weeklyplanner/WeekdayActivity.kt
+++ app/src/main/java/com/scappworks/weeklyplanner/WeekdayActivity.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.scappworks.weeklyplanner.databinding.ActivityMainBinding
 import com.scappworks.weeklyplanner.databinding.ActivityWeekdayBinding
 import com.scappworks.weeklyplanner.recyclerviews.TaskRvAdapter
+import com.scappworks.weeklyplanner.roomdb.tasktable.Task
 import com.scappworks.weeklyplanner.viewmodel.PlannerViewModel
 import com.scappworks.weeklyplanner.viewmodel.PlannerViewModelFactory
 
@@ -34,27 +35,41 @@ class WeekdayActivity : AppCompatActivity() {
 
         plannerViewModel.allWeekdays.observe(this, {weekdays ->
             weekdays?.let {
-                weekdays.forEach {
-                    if (it.id == selectedDay) {
-                        header.text = it.day
+                weekdays.forEach { weekday ->
+                    // If the day ID passed in matches the day being looked at...
+                    if (weekday.id == selectedDay) {
+                        // Set the header to the correct day, and start looking through tasks
+                        header.text = weekday.day
+
+                        plannerViewModel.allTasks.observe(this,  { tasks ->
+                            tasks?.let {
+                                // If tasks exist in the db
+                                if (tasks.count() > 0) {
+                                    // List of tasks for this day
+                                    val dayTasks: MutableList<Task> = mutableListOf()
+
+                                    tasks.forEach { task ->
+                                        /* If the weekday ID of the task matches the ID of
+                                        * the current day being looked at by the preceding
+                                        * weekday foreach loop, add that task to the list
+                                        * that will be presented */
+                                        if (task.weekdayId == weekday.id) {
+                                            dayTasks.add(task)
+                                        }
+                                    }
+                                    taskRvAdapter.submitList(dayTasks)
+                                    // Hide no tasks view and show task recyclerview
+                                    binding.noTasks.visibility = View.GONE
+                                    binding.tasksRv.visibility = View.VISIBLE
+                                }
+                                else {
+                                    // Hide task recyclerview and show no tasks view
+                                    binding.noTasks.visibility = View.VISIBLE
+                                    binding.tasksRv.visibility = View.GONE
+                                }
+                            }
+                        })
                     }
-                }
-            }
-        })
-
-        plannerViewModel.allTasks.observe(this,  { tasks ->
-            tasks?.let {
-                if (tasks.count() > 0) {
-
-                    // Query db for weekday
-
-                    binding.noTasks.visibility = View.GONE
-                    binding.tasksRv.visibility = View.VISIBLE
-                    taskRvAdapter.submitList(tasks)
-                }
-                else {
-                    binding.noTasks.visibility = View.VISIBLE
-                    binding.tasksRv.visibility = View.GONE
                 }
             }
         })
